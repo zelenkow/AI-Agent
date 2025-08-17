867a516adc661d338d29544c2fa9beaeeffea681
zelenkow <zelenkow@icloud.com>
2025-08-17 21:21:28 +0300

    fix_skelet_of_bot

--- bot.py
+++ bot.py
@@ -4,10 +4,15 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 load_dotenv()
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Привет! Я работаю!")
 
-app = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+def main() -> None:
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
