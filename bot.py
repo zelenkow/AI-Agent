3ef0ea7e5c9822b91a7e174e27e267271294dc75
zelenkow <zelenkow@icloud.com>
2025-08-22 22:51:45 +0300

    add_get_token_add_goals

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import aiohttp
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
@@ -9,21 +10,36 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CLIENT_ID = os.getenv("AVITO_CLIENT_ID")
+CLIENT_SECRET = os.getenv("AVITO_CLIENT_SECRET")
+DIKON_ID = os.getenv("DIKON_USER_ID")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command("support"))
-async def support(message: types.Message):
-    await message.answer("Служба поддержки временно не работает :)")
+async def get_avito_token():
+    data_api = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'grant_type': 'client_credentials'
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            "https://api.avito.ru/token",
+            data=data_api,
+        ) as response:
+            token_data = await response.json()
+            return token_data["access_token"]
 
 @dp.message(Command("report"))
 async def report(message: types.Message):
-    await message.answer("Бот работает!")    
+    token = await get_avito_token()
+    await message.answer(f"Токен получен: {token}") 
 
 @dp.message()
 async def send_way(message: types.Message):
-    await message.answer("Для формирования отчета, нажмите кнопку Меню")    
+    #print(message.model_dump_json(indent=4, exclude_none=True))  Для вывода JSON только с активными параметрами
+    await message.answer("Для формирования отчета, нажмите Меню и выберите Сформировать отчет")
 
 if __name__ == "__main__":
     dp.run_polling(bot)
